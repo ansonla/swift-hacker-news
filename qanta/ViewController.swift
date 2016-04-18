140a8153cada6b201ca20af4bedc9bd7724ecd7c
Cheuk Man Ng <cheuk@creasis-studios.com>
2016-04-18 18:55:50 +0100

    Initial Commit

---
+++ qanta/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  qanta
+//
+//  Created by Creasis Studios on 18/04/2016.
+//  Copyright © 2016 Chuck Man. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
